125fa6d9c7d84c44f6fa404d2059e74a44e268b8
LeonardoFornaro1 <91632279+LeonardoFornaro1@users.noreply.github.com>
2021-11-14 18:49:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   int z;
-   cin >> z;
-   int primo=0;
-   int secondo=1;
-   int terzo=1;
+    int z;
+    cin >> z;
+    int primo=0;
+    int secondo=1;
+    int terzo=1;
 
-   if(z>=2){
-   for(int i=0; i<z; ++i){
+    if(z>=2){
+    for(int i=0; i<z; ++i){
         cout << terzo << endl;
         terzo=primo+secondo;
         primo=secondo;
         secondo=terzo;
-   }
-   }
-   else{
+    }
+    }
+    else{
         cout << "errore" << endl;
-   }
-   return 0;
+    }
+
+    return 0;
 }
